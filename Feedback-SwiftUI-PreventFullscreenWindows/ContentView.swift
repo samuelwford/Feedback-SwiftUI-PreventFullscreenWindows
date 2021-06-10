43e4775bcfc9d76b749112093bda5c2b0cdd8903
Samuel Ford <samuel.ford@icloud.com>
2021-06-10 09:18:49 -0400

    Initial Commit

---
+++ Feedback-SwiftUI-PreventFullscreenWindows/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Feedback-SwiftUI-PreventFullscreenWindows
+//
+//  Created by Samuel Ford on 6/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Feedback-SwiftUI-PreventFullscreenWindows/Feedback_SwiftUI_PreventFullscreenWindowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feedback_SwiftUI_PreventFullscreenWindowsApp.swift
+//  Feedback-SwiftUI-PreventFullscreenWindows
+//
+//  Created by Samuel Ford on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Feedback_SwiftUI_PreventFullscreenWindowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
